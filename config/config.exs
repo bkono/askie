d11af1aaa52018a5f4ae8256e3160e35cd9b0296
Bryan Konowitz <bryan@kono.sh>
2015-12-12 13:32:21 -0800

    cleanup formatting

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,7 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :askie, application_id: System.get_env("ASKIE_APPLICATION_ID")
-
-config :askie, replay_attack_tolerance:
-  (System.get_env("ASKIE_REPLAY_ATTACK_TOLERANCE_IN_SECONDS") || "150")
+config :askie,
+  application_id: System.get_env("ASKIE_APPLICATION_ID"),
+  replay_attack_tolerance: (System.get_env("ASKIE_REPLAY_ATTACK_TOLERANCE_IN_SECONDS") || "150")
 
 import_config "#{Mix.env}.exs"
